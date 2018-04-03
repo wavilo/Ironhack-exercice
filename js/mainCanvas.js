8ce37a418cc537cb39607930c740375f1a79a463
Wavilo <bertrand.uhart@gmail.com>
2018-04-03 16:34:02 +0200

    mvp: main functionnalities expect the speed

--- js/mainCanvas.js
+++ js/mainCanvas.js
@@ -4,8 +4,9 @@ var ctx = canvas.getContext("2d");
 var ballRadius = 9;
 var x = 300;
 var y = 200;
-var dx = 2;
-var dy = -2;
+var dx = 1;
+var dy = 1;
+var speed = 0;
 var brickRowCount = 2;
 var brickColumnCount = 6;
 var brickWidth = 75;
@@ -14,11 +15,43 @@ var brickPadding = 10;
 var brickOffsetTop = 100;
 var brickOffsetLeft = 100;
 
+if (score === undefined) {
+  var score = 0;
+} else {
+  var score = 12;
+}
+
 var bricks = [];
 for (c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for (r = 0; r < brickRowCount; r++) {
     bricks[c][r] = { x: 0, y: 0, status: 1 }; //add the brickCounter
+  }
+}
+
+//this collision is working
+function collisionDetection() {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          if (score == brickRowCount * brickColumnCount) {
+            drawBricks();
+            collisionDetection();
+            score = 12;
+          }
+        }
+      }
+    }
   }
 }
 
@@ -45,20 +78,6 @@ function drawBricks() {
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
         ctx.fillStyle = "#c93424";
-        //test for the color of the bricks
-        // for (c = 0; c < brickColumnCount; c++) {
-        //   for (r = 0; r < brickRowCount; r++) {
-        //     var b = bricks[c][r];
-        //     if (b.brickCounter == 750) {
-        //       ctx.fillStyle = "#c93424";
-        //     } else if (b.brickCounter <= 749) {
-        //       ctx.fillStyle = "#74f442";
-        //     } else if (b.brickCounter <= 749) {
-        //       ctx.fillStyle = "#";
-        //     }
-        //   }
-        // }
-
         ctx.fill();
         ctx.closePath();
       }
@@ -66,52 +85,17 @@ function drawBricks() {
   }
 }
 
-//function for collision and brickCounter test
-// function collisionDetection() {
-//   for (c = 0; c < brickColumnCount; c++) {
-//     for (r = 0; r < brickRowCount; r++) {
-//       var b = bricks[c][r];
-//       if (b.brickCounter > 0) {
-//         if (
-//           x > b.x &&
-//           x < b.x + brickWidth &&
-//           y > b.y &&
-//           y < b.y + brickHeight
-//         ) {
-//           dy = -dy;
-//           b.brickCounter = b.brickCounter - 1;
-//         }
-//       } else if ((b.brickCounter = 0)) {
-//         b.status = 0;
-//       }
-//     }
-//   }
-// }
-
-//this collision is working
-function collisionDetection() {
-  for (c = 0; c < brickColumnCount; c++) {
-    for (r = 0; r < brickRowCount; r++) {
-      var b = bricks[c][r];
-      if (b.status == 1) {
-        if (
-          x > b.x &&
-          x < b.x + brickWidth &&
-          y > b.y &&
-          y < b.y + brickHeight
-        ) {
-          dy = -dy;
-          b.status = 0;
-        }
-      }
-    }
-  }
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
   drawBall();
+  drawScore();
   collisionDetection();
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
--- js/miniCanvas.js
+++ js/miniCanvas.js
@@ -1,17 +1,100 @@
-// ////////////////////////
-// //mini game press a key
-// ////////////////////////
+// link to 30 questions https://opentdb.com/api.php?amount=30&difficulty=easy
 
-// var canvas = document.querySelector("#mini-canvas");
-// var ctx = canvas.getContext("2d");
+// 20180403131938
+// https://opentdb.com/api.php?amount=30&difficulty=easy
 
-// /////////////////////////
-// //mini game when we have to click 30 times
-// ////////////////////////
-// var minecraftBrick = new Image();
-// minecraftBrick.src = "./images/ore-gold.png";
+//Questions
 
-// minecraftBrick.onload = function() {
-//   //ctx.drawImage(image, x, y, width, height)
-//   ctx.drawImage(minecraftBrick, 600, 200, 100, 100);
-// };
+var allQuestion = [
+  {
+    question:
+      "The name of the main character of the video game &quot;The Legend of Zelda&quot;, is Zelda.",
+    correct_answer: "False",
+    incorrect_answers: "True"
+  },
+  {
+    question: "Nintendo started out as a playing card manufacturer.",
+    correct_answer: "True",
+    incorrect_answers: "False"
+  },
+  {
+    question: "Igneous rocks are formed by excessive heat and pressure.",
+    correct_answer: "False",
+    incorrect_answers: "True"
+  }
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+  //   {
+  //     question:,
+  //     correct_answer: ,
+  //     incorrect_answers: ["True"]
+  //   },
+];
+
+// $(".true").click(function() {
+//   console.log("yolo");
+// });
+
+//function to launch a question
+function randomQuiz() {
+  var randomNb = Math.floor(Math.random() * 3); // random number on the array
+  document.querySelector(".mini-game h2").innerHTML =
+    allQuestion[randomNb].question;
+
+  $(".btn-reponse").click(function() {
+    // console.log($(this).hasClass("true"));
+    if (
+      allQuestion[randomNb].correct_answer == "True" &&
+      $(this).hasClass("true")
+    ) {
+      $(".congrats-message").html(
+        "Good answer, you increase the speed of the ball"
+      );
+      speed = speed + 1;
+    } else if (
+      allQuestion[randomNb].correct_answer == "True" &&
+      $(this).hasClass("false")
+    ) {
+      $(".congrats-message").html("Bad answer");
+    } else if (
+      allQuestion[randomNb].correct_answer == "False" &&
+      $(this).hasClass("false")
+    ) {
+      $(".congrats-message").html(
+        "Good answer, you increase the speed of the ball"
+      );
+      speed = speed + 1;
+    } else if (
+      allQuestion[randomNb].correct_answer == "False" &&
+      $(this).hasClass("true")
+    ) {
+      $(".congrats-message").html("Bad answer");
+    }
+  });
+}
+
+//setinterval
+setInterval(randomQuiz, 5000);
